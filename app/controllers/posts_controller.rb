524ce729732812c7c2a8ff37cf733d3b692904fa
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-11-10 19:24:14 +0900

    コントローラアクション記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,54 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
+    @posts = Post.all
   end
 
   def new
+    @posts = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "つぶやきを投稿しました！"
+      else
+        render :new
+      end
+    end
   end
 
   def edit
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "投稿を編集しました！"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"投稿を削除しました！"
   end
 
   def confirm
+    @post = post.new(post_params)
+    render :new if @post.invalid?
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def set_post
+    @post = post.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
-  get 'posts/confirm'
-  get 'users/new'
-  get 'users/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts do
+    collection do
+      post :confirm
+    end
+  end
 end
